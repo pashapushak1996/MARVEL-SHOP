1c5abe7fe4ddb82f8171120d0881495b01c5117e
pashapushak1996 <pashapushak1996@gmail.com>
2023-01-22 18:05:45 +0200

    feat: set up eslint and prettier

--- src/components/select/Select.tsx
+++ src/components/select/Select.tsx
@@ -1,35 +1,36 @@
-import React from "react";
+import React from 'react';
 
 import './Select.scss';
 
+
 export interface ISelectOption {
-    label: string;
-    value: string | number;
+  label: string;
+  value: string | number;
 }
 
 interface ISelectProps {
-    name?: string;
-    value?: ISelectOption;
-    onChange?: (event: React.ChangeEvent<HTMLSelectElement>) => void;
-    placeholder: string;
-    options: Array<ISelectOption>;
+  name?: string;
+  value?: ISelectOption;
+  onChange?: (event: React.ChangeEvent<HTMLSelectElement>) => void;
+  placeholder: string;
+  options: Array<ISelectOption>;
 }
 
 export const Select: React.FC<ISelectProps> = (props) => {
-    const {
-        name,
-        onChange,
-        options,
-        placeholder
-    } = props;
+  const {
+    name,
+    onChange,
+    options,
+    placeholder,
+  } = props;
 
-    return (
-        <select name={name} onChange={onChange} className='select'>
-            <option value={''}>{placeholder}</option>
-            {options.map((option, index) => {
-                return <option key={index} value={option.value}>{option.label}</option>
-            })
-            }
-        </select>
-    );
+  return (
+    <select name={name} onChange={onChange} className='select'>
+      <option value={''}>{placeholder}</option>
+      {options.map((option, index) => {
+        return <option key={index} value={option.value}>{option.label}</option>;
+      })
+      }
+    </select>
+  );
 };
